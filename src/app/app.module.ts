222f8a16844fca9b83b9a629cd306934bd812719
Michael Travers <michael.travers@udc.edu>
2024-03-25 19:32:11 -0400

    work in progress

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './component/nav/nav.component';
+import { BannerComponent } from './component/banner/banner.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    BannerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
